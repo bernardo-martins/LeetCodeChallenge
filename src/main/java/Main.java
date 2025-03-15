4feef6a799646f556fef4191baec90da12dccada
bernardo-martins <bme.martins@campus.fct.unl.pt>
2025-03-15 14:56:35 +0100

    Day4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,21 @@ import java.util.*;
 
 public class Main {
 
+    /*
+        Input: n = 12
+        Output: true
+        Explanation: 12 = 3*1 + 3*2
+     */
+    public static boolean checkPowersOfThree(int n) {
 
+        Stack<Integer> stack = new Stack<Integer>();
+
+
+
+        return Math.log(n) == 3;
+    }
 
     public static void main(String[] args) {
-
+        System.out.println(checkPowersOfThree(3));
     }
 }
---
+++ src/main/java/days/Day4.java
@@ -0,0 +1,9 @@
+package days;
+
+public class Day4 {
+
+    public boolean checkPowersOfThree(int n) {
+        return Math.log(n) == 3;
+    }
+
+}
