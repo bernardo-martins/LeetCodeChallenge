1200679f710a7de616c0230f51f84a50a5a21ebe
bernardo-martins <bme.martins@campus.fct.unl.pt>
2025-03-15 15:22:28 +0100

    Day4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,21 +3,16 @@ import java.util.*;
 
 public class Main {
 
-    /*
-        Input: n = 12
-        Output: true
-        Explanation: 12 = 3*1 + 3*2
-     */
-    public static boolean checkPowersOfThree(int n) {
-
-        Stack<Integer> stack = new Stack<Integer>();
-
-
-
-        return Math.log(n) == 3;
+    public static int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] >= target)
+                return i;
+        return nums.length;
     }
 
     public static void main(String[] args) {
-        System.out.println(checkPowersOfThree(3));
+        int[] nums = new int[] {1,3,5,6};
+        int target = 0;
+        System.out.println(searchInsert(nums, target));
     }
 }
--- src/main/java/days/Day4.java
+++ src/main/java/days/Day4.java
@@ -3,7 +3,19 @@ package days;
 public class Day4 {
 
     public boolean checkPowersOfThree(int n) {
-        return Math.log(n) == 3;
+        if(n == 0)
+            return false;
+        if(n == (Math.log(n) / Math.log(3)))
+            return true;
+        int i = 0;
+        return false;
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] >= target)
+                return i;
+        return nums.length;
     }
 
 }
