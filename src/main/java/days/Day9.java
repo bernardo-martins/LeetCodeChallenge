5eb176496f9f3c5c9307592efc209dc3996d9d6e
bernardo-martins <bme.martins@campus.fct.unl.pt>
2025-03-20 14:33:02 +0100

    Day9

--- src/main/java/days/Day9.java
+++ src/main/java/days/Day9.java
@@ -8,4 +8,15 @@ public class Day9 {
             return n;
         return climbStairs(n-1) + climbStairs(n-2);
     }
+
+    public int climbStairsDynamicProgramming(int x) {
+        if(x<=2)
+            return x;
+        int[] stairs = new int[x];
+        stairs[0] = 1;
+        stairs[1] = 2;
+        for(int i = 2; i<x; i++)
+            stairs[i] = stairs[i-1] + stairs[i-2];
+        return stairs[x-1];
+    }
 }
