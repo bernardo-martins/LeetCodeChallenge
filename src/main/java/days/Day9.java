c2e987fc29cf450c112d2c81cffa3b06df877b7b
bernardo-martins <bme.martins@campus.fct.unl.pt>
2025-03-20 16:08:59 +0100

    Day9

--- src/main/java/days/Day9.java
+++ src/main/java/days/Day9.java
@@ -1,5 +1,10 @@
 package days;
 
+import java.lang.reflect.Array;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 public class Day9 {
 
     public int climbStairs(int n) {
@@ -22,5 +27,15 @@ public class Day9 {
 
     public int mySqrt(int x) {
         return Integer.valueOf((int) Math.sqrt((double)x));
+    }
+
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i  = 0 ; i < nums.length; i++) {
+            if(!set.add(nums[i])) {
+                set.remove(nums[i]);
+            }
+        }
+        return set.iterator().next();
     }
 }
