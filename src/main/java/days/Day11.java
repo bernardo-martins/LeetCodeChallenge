6494df5441b2110849c6e741b244007175b59933
bernardo-martins <bme.martins@campus.fct.unl.pt>
2025-03-22 14:38:42 +0100

    Day11

--- src/main/java/days/Day11.java
+++ src/main/java/days/Day11.java
@@ -13,4 +13,13 @@ public class Day11 {
     private int getDepth(TreeNode node) {
         return 0;
     }
+
+    public void reverseString(char[] s) {
+        char aux = ' ';
+        for(int i = 0; i < s.length/2; i++) {
+            aux = s[s.length-i-1];
+            s[s.length-i-1] = s[i];
+            s[i] = aux;
+        }
+    }
 }
