57ebb91ec23a673da20c31bc9bac834780296491
bernardo-martins <bme.martins@campus.fct.unl.pt>
2025-03-22 14:35:17 +0100

    Day11

---
+++ src/main/java/days/Day11.java
@@ -0,0 +1,16 @@
+package days;
+
+import utils.TreeNode;
+
+import java.util.Stack;
+
+public class Day11 {
+
+    public boolean isBalanced(TreeNode root) {
+
+        return true;
+    }
+    private int getDepth(TreeNode node) {
+        return 0;
+    }
+}
