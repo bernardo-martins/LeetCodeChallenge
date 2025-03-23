c944f368e755cbf4524c187d15ccf93269a2e384
bernardo-martins <bme.martins@campus.fct.unl.pt>
2025-03-23 19:29:42 +0100

    Day12

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,27 @@
 
 public class Main {
 
+    public static String addBinary(String a, String b) {
+        int resA = 0;
+        char[] arrayA = a.toCharArray();
+        for(int i = 0; i < arrayA.length; i++) {
+            if(arrayA[i] != '0')
+                resA+=Math.pow(2,i);
+        }
+
+        int resB = 0;
+        char[] arrayB = b.toCharArray();
+        for(int i = 0; i < arrayB.length; i++) {
+            if(arrayB[i] != '0')
+                resB+=Math.pow(2,i);
+        }
+        int res = resA+resB;
+        return Integer.toBinaryString(res);
+    }
+
 
     public static void main(String[] args) {
+        System.out.println(addBinary("11", "1"));
 
     }
 }
--- src/main/java/days/Day12.java
+++ src/main/java/days/Day12.java
@@ -12,4 +12,21 @@ public class Day12 {
         int rightDepth = maxDepth(root.right); // Depth of the right subtree
         return Math.max(leftDepth, rightDepth) + 1; // Max depth + 1 for the current node
     }
+
+    public String addBinary(String a, String b) {
+        int resA = 0;
+        char[] arrayA = a.toCharArray();
+        for(int i = 0; i < arrayA.length; i++) {
+            if(arrayA[i] != '0')
+                resA+=Math.pow(2,i);
+        }
+
+        int resB = 0;
+        char[] arrayB = b.toCharArray();
+        for(int i = 0; i < arrayB.length; i++) {
+            if(arrayB[i] != '0')
+                resB+=Math.pow(2,i);
+        }
+        return Integer.toBinaryString(resA+resB);
+    }
 }
